6c77abe08111ea01426110e44f082061649ce5d2
VenomC312 <63124013+venomc312@users.noreply.github.com>
2021-07-09 23:58:29 -0400

    Status command updates for embeds
    +color.cr - This creates the ability to convert RGB to Hex inside of the embed color option.
    
    -status.cr - Now uses embeds instead of normal messages.

---
+++ vcbot/src/color.cr
@@ -0,0 +1,26 @@
+require "json"
+
+struct Color
+    include JSON::Serializable
+
+    def initialize(@r : UInt8, @g : UInt8, @b : UInt8)
+    end
+
+    def to_u : UInt32
+        return (@r.to_u<<16_u8) + (@g.to_u<<8_u8) + @b.to_u
+    end
+end
+struct ColorA
+    include JSON::Serializable
+
+    def initialize(@r : UInt8, @g : UInt8, @b : UInt8, @a : UInt8)
+    end
+
+    def to_u : UInt32
+        return (@a.to_u<<24_u8) + (@r.to_u<<16_u8) + (@g.to_u<<8_u8) + @b.to_u
+    end
+end
+
+#puts Color.new(255,255,255).to_u
+#puts 0xFFFFFF
+#puts ColorA.new(255,255,255,127).to_u
--- vcbot/src/commands/status.cr
+++ vcbot/src/commands/status.cr
@@ -1,4 +1,5 @@
 require "../../lib/discordcr/src/discordcr"
+require "../color"
 require "../discord"
 require "json"
 require "http/client"
@@ -8,9 +9,19 @@ module Commands
     def status(payload) : Nil
         response = HTTP::Client.get "http://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=69.30.205.147&format=json"
         if response.body.includes?("garrysmod")
-            Discord.client.create_message(payload.channel_id, "VTB is currently up!")
+            Discord.client.create_message(payload.channel_id, "", Discord::Embed.new(
+                title: "Venom's Train Build",
+                description: "Online",
+                timestamp: Time.utc,
+                colour: Color.new(0,255,81).to_u
+            ))
         else
-            Discord.client.create_message(payload.channel_id, "VTB is currently down!")
+            Discord.client.create_message(payload.channel_id, "", Discord::Embed.new(
+                title: "Venom's Train Build",
+                description: "Offline",
+                timestamp: Time.utc,
+                colour: Color.new(255,0,0).to_u
+            ))
         end
     end
     Discord.commands["status"] = ->status(Discord::Message)
