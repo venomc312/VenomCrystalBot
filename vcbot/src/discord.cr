110bc0213fea9422f8a70d502a6aa38c2f749af2
VenomC312 <63124013+venomc312@users.noreply.github.com>
2021-07-10 00:27:15 -0400

    updates instance + proj name

--- vcbot/src/discord.cr
+++ vcbot/src/discord.cr
@@ -1,6 +1,3 @@
-# This simple example bot replies to every "!ping" message with "Pong!".
-
-
 require "../lib/discordcr/src/discordcr"
 require "./jsonreader"
 require "./commands/*"
@@ -8,13 +5,13 @@ require "./commands/*"
 # Import prefix from json (Prefix is defined as an export in jsonreader.cr)
 prefix = JSONReader.test.prefix
 
-# Command name must be the same as the module name for this command handler to work!
+# Refer to one of the existing commands in ./commands/ to see how to add a command
 module Discord
   class_getter client : Discord::Client {Discord::Client.new(token: "Bot #{JSONReader.test.token}")}
   class_getter commands : Hash(String, Proc(Discord::Message, Nil)) { Hash(String, Proc(Discord::Message, Nil)).new }
 end
 
-# This event isn't being triggered and I don't know why :(
+# This event will only be triggered with server member intents enabled
 Discord.client.on_guild_member_add do |payload|
   Discord.client.add_guild_member_role(guild_id: 850173983806849034.to_u64, user_id: payload.user.id, role_id: 861485216375242772.to_u64)
 end
@@ -28,8 +25,7 @@ Discord.client.on_message_create do |payload|
       cmdname = args[0].gsub(prefix, "")
 
       if Discord.commands.has_key?(cmdname)
-        puts "Command in cmd array!"
-        puts "Running command: #{cmdname}"
+        puts "\nCommand in cmd array!\nRunning command: #{cmdname}\nRan by #{payload.author.username} on server-id #{payload.guild_id}\n "
 
         spawn name: "cmd" do
             Discord.commands[cmdname].call(payload)
