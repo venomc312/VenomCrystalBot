7595a1c9a27c144bdefad20f09c56f8a00fa914a
VenomC312 <63124013+venomc312@users.noreply.github.com>
2021-07-12 08:48:10 -0400

    Added MC-Server Status

--- vcbot/src/commands/status.cr
+++ vcbot/src/commands/status.cr
@@ -8,6 +8,7 @@ module Commands
     extend self
     def status(payload) : Nil
         response = HTTP::Client.get "http://api.steampowered.com/ISteamApps/GetServersAtAddress/v0001?addr=69.30.205.147&format=json"
+        response1 = HTTP::Client.get "https://mcapi.us/server/status?ip=mc1.venomc312.com"
         if response.body.includes?("garrysmod")
             Discord.client.create_message(payload.channel_id, "", Discord::Embed.new(
                 title: "Venom's Train Build",
@@ -18,6 +19,25 @@ module Commands
         else
             Discord.client.create_message(payload.channel_id, "", Discord::Embed.new(
                 title: "Venom's Train Build",
+                description: "Offline",
+                timestamp: Time.utc,
+                colour: Color.new(255,0,0).to_u
+            ))
+        end
+
+        if response1.body.includes?("true")
+            json = JSON.parse(response1.body)
+            players = json["players"]
+
+            Discord.client.create_message(payload.channel_id, "", Discord::Embed.new(
+                title: "Venom's Minecraft Server",
+                description: "Players: " + players["now"].to_s + "/" + players["max"].to_s,
+                timestamp: Time.utc,
+                colour: Color.new(0,255,81).to_u
+            ))
+        else
+            Discord.client.create_message(payload.channel_id, "", Discord::Embed.new(
+                title: "Venom's Minecraft Server",
                 description: "Offline",
                 timestamp: Time.utc,
                 colour: Color.new(255,0,0).to_u
